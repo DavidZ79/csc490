6a2cc7ef19193cb5b9eb080f762fe183b8b0630f
David Zhou <david.m8f.z@gmail.com>
2024-04-10 23:52:39 -0400

    background change selection (without persistance)

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,23 +1,30 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { useFrame } from "react-three-fiber";
 import * as THREE from "three";
 import { useControls } from "leva";
 
-
 const Background = () => {
+    
+    // State to hold the current background option
+    const [selectedBackground, setSelectedBackground] = useState("firstBackground");
 
     // leva UI controls
-    const { background } = useControls( {
+    const { background } = useControls({
         background: {
-            value: "first",
-            options: ["first", "second"],
+            value: "firstBackground",
+            options: ["firstBackground", "secondBackground"],
         },
-    })
+    });
 
-    /* import image */
-    const texture = useTexture("textures/beach.jpg");
+    // useEffect to listen for changes in the background option
+    useEffect(() => {
+        setSelectedBackground(background);
+    }, [background]);
+
+    // Load different textures based on the selected background
+    const texture = useTexture(`textures/${selectedBackground}.jpg`);
 
     /* viewport object */
     const viewport = useThree((state) => state.viewport);
@@ -28,7 +35,6 @@ const Background = () => {
             <planeGeometry args={[viewport.width, viewport.height]} />
             {/* place image on plane */}
             <meshBasicMaterial map={texture} />
-
         </mesh>
     );
 };
